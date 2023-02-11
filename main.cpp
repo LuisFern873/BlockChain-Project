87a87449facc2e5e6c30a1155ffb154b8f1c2cd7
Luis Fernando Mendez Lazaro <luis.mendez.l@utec.edu.pe>
2023-02-11 11:18:23 -0500

    menu avance

--- main.cpp
+++ main.cpp
@@ -5,45 +5,29 @@
 
 using namespace std;
 
-
-/*
-    BlockChain<Transfer> chain;
-
-    chain.insert(Transfer(100, "Luis", "Pierina"));
-    chain.insert(Transfer(20, "Stuart", "Mario"));
-    chain.insert(Transfer(50, "Luciana", "Pablo"));
-    chain.insert(Transfer(80, "Pedro", "Andy"));
-    chain.insert(Transfer(60, "Carlos", "Carlota"));
-
-    chain.display();
-
-    Sender Hash Index
-    Indexing::sender_hash_index<Transfer>(chain);
-*/
-
 int main()
 {
-    // Load data
-    BlockChain<Transfer> chain;
+    // // Load data
+    // BlockChain<Transfer> chain;
 
-    chain.insert(Transfer(100, "Luis", "Pierina"));
-    chain.insert(Transfer(20, "Stuart", "Mario"));
-    chain.insert(Transfer(50, "Luciana", "Pablo"));
-    chain.insert(Transfer(80, "Pedro", "Andy"));
-    chain.insert(Transfer(60, "Carlos", "Carlota"));
+    // chain.insert(Transfer(100, "Luis", "Pierina"));
+    // chain.insert(Transfer(20, "Stuart", "Mario"));
+    // chain.insert(Transfer(50, "Luciana", "Pablo"));
+    // chain.insert(Transfer(80, "Pedro", "Andy"));
+    // chain.insert(Transfer(60, "Carlos", "Carlota"));
 
-    chain.display();
+    // chain.display();
 
-    // Sender and Receiver Hash Index
-    Index index;
-    index.create_hash_index(chain);
+    // // Sender and Receiver Hash Index
+    // Index index;
+    // index.create_hash_index(chain);
 
-    cout << index.EqualTo(Member::sender, "Pedro");
-    cout << index.EqualTo(Member::receiver, "Mario");
+    // cout << index.EqualTo(Member::sender, "Pedro");
+    // cout << index.EqualTo(Member::receiver, "Mario");
 
     // Display menu
-    // auto Menu = Menu::init();
-    // Menu->display();
+    auto Menu = Menu::init();
+    Menu->display_main();
 
     return 0;
 }
--- menu.h
+++ menu.h
@@ -8,57 +8,121 @@ using namespace std;
 class Menu
 {
     private:
-    inline static Menu* instance = nullptr;
-
-    Menu() = default;
+        inline static Menu* instance = nullptr;
+        Menu() = default;
+        struct Capybara
+        {
+            static void display();
+        };
 
     public:
-    inline static Menu* init()
-    {
-        if(instance == nullptr) 
-            instance = new Menu();
-        return instance;
-    }
-    void display()
-    {
-        short option;
-        cout << "********** Welcome to CapyCoin **********\n";
-        cout << "1) Create Transaction 💰\n";
-        cout << "2) Query data 📊\n";
-        cout << "3) Sign out ❌\n";
+        static Menu* init();
+        void display_main();
 
-        cin >> option;
-        
-        switch (option) 
-        {
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            cout << "Thank you for your preference.\n";
-            break;
-        default:
-            cout << "Enter a valid option.\n";
-            break;
-        }
-    }
+    private:
+        void display_create();
+        void display_query();
 };
 
-/*
-000000000000000000000000000000000000
-⠀⠀⢀⣀⠤⠿⢤⢖⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡔⢩⠂⠀⠒⠗⠈⠀⠉⠢⠄⣀⠠⠤⠄⠒⢖⡒⢒⠂⠤⢄⠀⠀⠀⠀
-⠇⠤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠈⠀⠈⠈⡨⢀⠡⡪⠢⡀⠀
-⠈⠒⠀⠤⠤⣄⡆⡂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠢⠀⢕⠱⠀
-⠀⠀⠀⠀⠀⠈⢳⣐⡐⠐⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠁⠇
-⠀⠀⠀⠀⠀⠀⠀⠑⢤⢁⠀⠆⠀⠀⠀⠀⠀⢀⢰⠀⠀⠀⡀⢄⡜⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠘⡦⠄⡷⠢⠤⠤⠤⠤⢬⢈⡇⢠⣈⣰⠎⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⣃⢸⡇⠀⠀⠀⠀⠀⠈⢪⢀⣺⡅⢈⠆⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠶⡿⠤⠚⠁⠀⠀⠀⢀⣠⡤⢺⣥⠟⢡⠃⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+Menu* Menu::init()
+{
+    if(instance == nullptr) 
+        instance = new Menu();
+    return instance;
+}
 
-*/
+void Menu::display_main()
+{
+    cout << "-----------------------------------------\n";
+    cout << "********** Welcome to CapyCoin **********\n";
+    cout << "-----------------------------------------\n";
+    Capybara::display();
+    cout << "1) Create transaction 💰\n";
+    cout << "2) Query data 📊\n";
+    cout << "3) Sign out ❌\n";
+    cout << "Enter option: ";
+    short option;
+    cin >> option;
+    cout << "\n";
+    
+    switch (option) 
+    {
+    case 1:
+        display_create();
+        break;
+    case 2:
+        display_query();
+        break;
+    case 3:
+        cout << "Thank you for your preference!\n";
+        break;
+    default:
+        cout << "Enter a valid option.\n";
+        break;
+    }
+    
+}
+
+void Menu::display_create()
+{
+    cout << "----------------------------------------\n";
+    cout << "********** Create transaction ********** \n";
+    cout << "----------------------------------------\n";
+    cout << "1) Make transfer 🟢\n";
+    cout << "2) Deposit 🟡\n";
+    cout << "3) Withdrawal 🟣\n";
+    cout << "4) Go back to main menu ⬅️\n";
+    cout << "Enter option: ";
+    short option;
+    cin >> option;
+    switch (option)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        display_main();
+        break;
+    default:
+        break;
+    }
+}
+
+void Menu::display_query()
+{
+    cout << "--------------------------------\n";
+    cout << "********** Query data **********\n";
+    cout << "--------------------------------\n";
+    cout << "1) Equal to ...\n";
+    cout << "2) Range search ...\n";
+    cout << "3) Start with ...\n";
+    cout << "4) Contains ...\n";
+    cout << "5) Max value ...\n";
+    cout << "6) Min value ...\n";
+    cout << "7) Go back to main menu ⬅️\n";
+    cout << "Enter option: ";
+    short option;
+    cin >> option;
+}
+
+void Menu::Capybara::display()
+{
+    cout << "⠀⠀⢀⣀⠤⠿⢤⢖⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n";
+    cout << "⡔⢩⠂⠀⠒⠗⠈⠀⠉⠢⠄⣀⠠⠤⠄⠒⢖⡒⢒⠂⠤⢄⠀⠀⠀⠀\n";
+    cout << "⠇⠤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠈⠀⠈⠈⡨⢀⠡⡪⠢⡀⠀\n";
+    cout << "⠈⠒⠀⠤⠤⣄⡆⡂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠢⠀⢕⠱⠀\n";
+    cout << "⠀⠀⠀⠀⠀⠈⢳⣐⡐⠐⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠁⠇\n";
+    cout << "⠀⠀⠀⠀⠀⠀⠀⠑⢤⢁⠀⠆⠀⠀⠀⠀⠀⢀⢰⠀⠀⠀⡀⢄⡜⠀\n";
+    cout << "⠀⠀⠀⠀⠀⠀⠀⠀⠘⡦⠄⡷⠢⠤⠤⠤⠤⢬⢈⡇⢠⣈⣰⠎⠀⠀\n";
+    cout << "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣃⢸⡇⠀⠀⠀⠀⠀⠈⢪⢀⣺⡅⢈⠆⠀⠀\n";
+    cout << "⠀⠀⠀⠀⠀⠀⠀⠶⡿⠤⠚⠁⠀⠀⠀⢀⣠⡤⢺⣥⠟⢡⠃⠀⠀⠀\n";
+    cout << "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀\n";
+}
+
+
 
 # endif // MENU_H
 
